ae9bab6ce4439a721ad9ccf5f2aab163f49e882a
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-10-12 15:53:01 -0500

    Code style definition

--- screens/Characters.js
+++ screens/Characters.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native';
 
 const Home = () => {
   return (
@@ -7,17 +7,17 @@ const Home = () => {
       <Text style={styles.text}>Characters Screen</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#292929",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#292929',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
-    color: "white",
+    color: 'white',
     fontSize: 30,
     marginBottom: 10,
   },
