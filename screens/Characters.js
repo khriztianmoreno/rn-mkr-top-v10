1cd65e6102207bfe38a33bf7ef9f884d27eb7b0f
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-10-13 18:05:37 -0500

    Add the searchbar

--- screens/Characters.js
+++ screens/Characters.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Text, View, StyleSheet, FlatList, Platform } from 'react-native';
+import debounce from 'lodash/debounce';
 
+import SearchBar from '../components/Search/SearchBar';
 import CharCard from '../components/Cards/Character';
 
 const Characters = () => {
   const [data, setData] = React.useState({
     characters: [],
-    currentPage: 1,
+    currentPage: 0,
     totalPages: 1,
   });
+
+  const searchCharacterHandler = async (text) => {
+    if (text) {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/?name=${text}`
+        );
+        const responseData = await response.json();
+        if (responseData.error) {
+          setData({
+            characters: [],
+            currentPage: 0,
+            totalPages: 1,
+            error: true,
+          });
+        } else {
+          setData({
+            characters: responseData.results,
+            currentPage: 1,
+            totalPages: responseData.info.pages,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const searchCharacterDebounced = React.useCallback(
+    debounce(searchCharacterHandler, 900),
+    []
+  );
 
   const getCharacters = async (page = 1) => {
     try {
@@ -28,21 +62,39 @@ const Characters = () => {
   };
 
   React.useEffect(() => {
+    console.log('Use Effect');
     getCharacters(1);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Characters Screen</Text>
-      <FlatList
-        data={data.characters}
-        renderItem={({ item }) => <CharCard {...item} />}
-        keyExtractor={(item) => item.id}
-        numColumns={2}
-        ListFooterComponent={() => <Text style={styles.text}>End of File</Text>}
-        onEndReachedThreshold={1}
-        onEndReached={() => getCharacters(data.currentPage + 1)}
-      />
+      <View style={styles.searchBar}>
+        <SearchBar
+          title="characters"
+          handleChangeInput={searchCharacterDebounced}
+        />
+      </View>
+      <View style={styles.list}>
+        {data?.error ? (
+          <View>
+            <Text style={styles.text}>There is nothing here</Text>
+          </View>
+        ) : (
+          <FlatList
+            data={data.characters}
+            renderItem={({ item }) => <CharCard {...item} />}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            ListFooterComponent={() => (
+              <Text style={styles.text}>End of File</Text>
+            )}
+            onEndReachedThreshold={30}
+            onEndReached={() => {
+              getCharacters(data.currentPage + 1);
+            }}
+          />
+        )}
+      </View>
     </View>
   );
 };
@@ -53,6 +105,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#292929',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  searchBar: {
+    flex: 1,
+  },
+  list: {
+    flex: 3,
   },
   text: {
     color: 'white',
